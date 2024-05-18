b00a73d9cd0302a3b113b9ec784657d0bc98ed63
Ntdi <yogikngyt@gmail.com>
2024-05-18 10:43:47 -0400

    random

---
+++ SpriteRandom.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class SpriteRandom : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		uint movement = GD.Randi() % 4;
+		const float AMOUNT = 10;
+		
+		if (movement == 0)
+		{
+			this.Position += new Vector2(0, -AMOUNT);
+		}
+		
+		if (movement == 1)
+		{
+			this.Position += new Vector2(0, AMOUNT);
+		}
+		
+		if (movement == 2)
+		{
+			this.Position += new Vector2(-AMOUNT, 0);
+		}
+		
+		if (movement == 3)
+		{
+			this.Position += new Vector2(AMOUNT, 0);
+		}
+	}
+}

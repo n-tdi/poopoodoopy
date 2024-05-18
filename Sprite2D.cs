f9ae33ef055649f0ae899c6fd83ed7c762f6374f
Ntdi <yogikngyt@gmail.com>
2024-05-18 10:30:55 -0400

    movement

--- Sprite2D.cs
+++ Sprite2D.cs
@@ -11,5 +11,26 @@ public partial class Sprite2D : Godot.Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		const float AMOUNT = 5;
+		
+		if (Input.IsKeyPressed(Key.W))
+		{
+			this.Position += new Vector2(0, -AMOUNT);
+		}
+		
+		if (Input.IsKeyPressed(Key.S))
+		{
+			this.Position += new Vector2(0, AMOUNT);
+		}
+		
+		if (Input.IsKeyPressed(Key.A))
+		{
+			this.Position += new Vector2(-AMOUNT, 0);
+		}
+		
+		if (Input.IsKeyPressed(Key.D))
+		{
+			this.Position += new Vector2(AMOUNT, 0);
+		}
 	}
 }
